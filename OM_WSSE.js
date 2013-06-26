057e3fab5792af5ee4d23ef107162e088674aca2
Brent Shaffer <bshafs@gmail.com>
2013-06-26 16:10:08 -0600

    updates OM_WSSE.js according to code standards

--- OM_WSSE.js
+++ OM_WSSE.js
@@ -1,42 +1,34 @@
 function OM_WSSE(u, s, n, c)
 {
-	this.set = function (u, s, n, c)
-	{
-		this.setUser(u);
-		this.setSecret(s);
-		this.setNonce(n);
-		this.setCreated(c);
-	};
+    this.set = function (u, s, n, c) {
+        this.setUser(u);
+        this.setSecret(s);
+        this.setNonce(n);
+        this.setCreated(c);
+    };
 
-	this.setUser = function (u)
-	{
-		this.u = u;
-	};
-	this.setSecret = function (s)
-	{
-		this.s = s;
-	};
-	this.setNonce = function (n)
-	{
-		this.n = n;
-	};
-	this.setCreated = function (c)
-	{
-		this.c = c;
-	};
+    this.setUser = function (u) {
+        this.u = u;
+    };
+    this.setSecret = function (s) {
+        this.s = s;
+    };
+    this.setNonce = function (n) {
+        this.n = n;
+    };
+    this.setCreated = function (c) {
+        this.c = c;
+    };
 
-	this.generateNonce = function ()
-	{
+    this.generateNonce = function () {
         var len = 24, chars  = "0123456789abcdef", nonce  = "";
-        for (var i = 0; i < len; i++)
-        {
+        for (var i = 0; i < len; i++) {
             nonce += chars.charAt(Math.floor(Math.random() * chars.length));
         }
         return nonce;
-	};
+    };
 
-	this.generateCreated = function ()
-	{
+    this.generateCreated = function () {
         var date = new Date(), y, m, d, h, i, s;
         y = date.getUTCFullYear();
         m = (date.getUTCMonth() + 1); if (m < 10) { m = "0" + m; }
@@ -47,53 +39,48 @@ function OM_WSSE(u, s, n, c)
         return y + "-" + m + "-" + d + "T" + h + ":" + i + ":" + s + "Z";
     };
 
-	this.encode = function ()
-	{
-		var p = {};
-		if (!window.b64_sha1)
-		{
-			alert("b64_sha1() was not found");
-			return p;
-		}
-		p.u = this.u;
-		p.s = this.s;
-		p.n = (this.n ? this.n : this.generateNonce());
-		p.c = (this.c ? this.c : this.generateCreated());
-		p.d = b64_sha1(p.n + p.c + p.s);
-		p.n = base64encode(p.n);
-		return p;
-	};
+    this.encode = function () {
+        var p = {};
+        if (!window.b64_sha1) {
+            alert("b64_sha1() was not found");
+            return p;
+        }
+        p.u = this.u;
+        p.s = this.s;
+        p.n = (this.n ? this.n : this.generateNonce());
+        p.c = (this.c ? this.c : this.generateCreated());
+        p.d = b64_sha1(p.n + p.c + p.s);
+        p.n = base64encode(p.n);
+        return p;
+    };
 
-	this.generateRESTHeaders = function ()
-	{
-		rval = "";
+    this.generateRESTHeaders = function () {
+        rval = "";
 
-		var p = this.encode();
+        var p = this.encode();
 
-		rval = "UsernameToken";
+        rval = "UsernameToken";
         rval += " Username=\"" + p.u + "\",";
         rval += " PasswordDigest=\"" + p.d + "\",";
         rval += " Nonce=\"" + p.n + "\",";
         rval += " Created=\"" + p.c + "\"";
 
-		return { 'X-WSSE': rval };
-	};
+        return { 'X-WSSE': rval };
+    };
 
-	this.generateRESTQS = function ()
-	{
-		var p = this.encode(), rval = "";
-		rval  = "auth_digest="    + escape(p.d);
+    this.generateRESTQS = function () {
+        var p = this.encode(), rval = "";
+        rval  = "auth_digest="    + escape(p.d);
         rval += "&auth_nonce="    + escape(p.n);
         rval += "&auth_created="  + escape(p.c);
         rval += "&auth_username=" + escape(p.u);
-		return rval;
-	};
+        return rval;
+    };
 
-	this.generateSOAPHeaders = function ()
-	{
-		var p = this.encode(), rval = "";
+    this.generateSOAPHeaders = function () {
+        var p = this.encode(), rval = "";
 
-		rval  = "<wsse:Security wsse:mustUnderstand=\"1\" xmlns:wsse=\"http://www.omniture.com\">\n";
+        rval  = "<wsse:Security wsse:mustUnderstand=\"1\" xmlns:wsse=\"http://www.omniture.com\">\n";
         rval += "\t<wsse:UsernameToken wsse:Id=\"User\">\n";
         rval += "\t\t<wsse:Username>" + p.u + "</wsse:Username>\n";
         rval += "\t\t\t<wsse:Password Type=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest\">" + p.d + "</wsse:Password>\n";
@@ -102,8 +89,8 @@ function OM_WSSE(u, s, n, c)
         rval += "\t</wsse:UsernameToken>\n";
         rval += "</wsse:Security>\n";
 
-		return rval;
-	};
+        return rval;
+    };
 
-	this.set(u, s, n, c);
+    this.set(u, s, n, c);
 }
--- base64.js
+++ base64.js
@@ -30,28 +30,28 @@ function base64encode(str) {
     i = 0;
     out = "";
     while(i < len) {
-	c1 = str.charCodeAt(i++) & 0xff;
-	if(i == len)
-	{
-	    out += base64EncodeChars.charAt(c1 >> 2);
-	    out += base64EncodeChars.charAt((c1 & 0x3) << 4);
-	    out += "==";
-	    break;
-	}
-	c2 = str.charCodeAt(i++);
-	if(i == len)
-	{
-	    out += base64EncodeChars.charAt(c1 >> 2);
-	    out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
-	    out += base64EncodeChars.charAt((c2 & 0xF) << 2);
-	    out += "=";
-	    break;
-	}
-	c3 = str.charCodeAt(i++);
-	out += base64EncodeChars.charAt(c1 >> 2);
-	out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
-	out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >>6));
-	out += base64EncodeChars.charAt(c3 & 0x3F);
+    c1 = str.charCodeAt(i++) & 0xff;
+    if(i == len)
+    {
+        out += base64EncodeChars.charAt(c1 >> 2);
+        out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+        out += "==";
+        break;
+    }
+    c2 = str.charCodeAt(i++);
+    if(i == len)
+    {
+        out += base64EncodeChars.charAt(c1 >> 2);
+        out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+        out += base64EncodeChars.charAt((c2 & 0xF) << 2);
+        out += "=";
+        break;
+    }
+    c3 = str.charCodeAt(i++);
+    out += base64EncodeChars.charAt(c1 >> 2);
+    out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+    out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >>6));
+    out += base64EncodeChars.charAt(c3 & 0x3F);
     }
     return out;
 }
@@ -64,44 +64,44 @@ function base64decode(str) {
     i = 0;
     out = "";
     while(i < len) {
-	/* c1 */
-	do {
-	    c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
-	} while(i < len && c1 == -1);
-	if(c1 == -1)
-	    break;
+    /* c1 */
+    do {
+        c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+    } while(i < len && c1 == -1);
+    if(c1 == -1)
+        break;
 
-	/* c2 */
-	do {
-	    c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
-	} while(i < len && c2 == -1);
-	if(c2 == -1)
-	    break;
+    /* c2 */
+    do {
+        c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+    } while(i < len && c2 == -1);
+    if(c2 == -1)
+        break;
 
-	out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
+    out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
 
-	/* c3 */
-	do {
-	    c3 = str.charCodeAt(i++) & 0xff;
-	    if(c3 == 61)
-		return out;
-	    c3 = base64DecodeChars[c3];
-	} while(i < len && c3 == -1);
-	if(c3 == -1)
-	    break;
+    /* c3 */
+    do {
+        c3 = str.charCodeAt(i++) & 0xff;
+        if(c3 == 61)
+        return out;
+        c3 = base64DecodeChars[c3];
+    } while(i < len && c3 == -1);
+    if(c3 == -1)
+        break;
 
-	out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
+    out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
 
-	/* c4 */
-	do {
-	    c4 = str.charCodeAt(i++) & 0xff;
-	    if(c4 == 61)
-		return out;
-	    c4 = base64DecodeChars[c4];
-	} while(i < len && c4 == -1);
-	if(c4 == -1)
-	    break;
-	out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+    /* c4 */
+    do {
+        c4 = str.charCodeAt(i++) & 0xff;
+        if(c4 == 61)
+        return out;
+        c4 = base64DecodeChars[c4];
+    } while(i < len && c4 == -1);
+    if(c4 == -1)
+        break;
+    out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
     }
     return out;
 }
